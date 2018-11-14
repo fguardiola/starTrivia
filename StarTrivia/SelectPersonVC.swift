57d6288743233dff4070876d7c24908ab2698a02
fguardiola <fernando.guardiola.888@gmail.com>
2018-11-14 11:03:33 +0100

    First commit. Add Structure & custom fonts

---
+++ StarTrivia/SelectPersonVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by 67621177 on 14/11/2018.
+//  Copyright © 2018 67621177. All rights reserved.
+//
+
+import UIKit
+
+class SelectPersonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
